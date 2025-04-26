4d7ddcd19ee7dee33cc59e7cdc7d9144d7780a95
Abhay Goud <abhayajay15@gmail.com>
2025-04-26 21:47:28 +0530

    created Homepage UI

--- Pages/Home/home.jsx
+++ Pages/Home/home.jsx
@@ -1,15 +1,15 @@
-import react from 'react'
-import SideNavbar from '../../Components/sideNavbar/sideNavbar'
-import './home.css'
-import HomePage from '../../Components/homePage/homePage'
+import react from "react";
+import SideNavbar from "../../Components/sideNavbar/sideNavbar";
+import "./home.css";
+import HomePage from "../../Components/homePage/homePage";
 
-const Home=({sideNavbar})=>{
+const Home = ({ sideNavbar }) => {
   return (
-    <div className='home'>
-        <SideNavbar sideNavbar={sideNavbar}/>
-        <HomePage/>
+    <div className="home">
+      <SideNavbar sideNavbar={sideNavbar} />
+        <HomePage sideNavbar={sideNavbar} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
